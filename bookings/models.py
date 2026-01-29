1a95f1a7bdf5527b8939417da13dd85e61ef03f7
Court-Pencil <courtneyhulse97@outlook.com>
2026-01-29 18:36:46 +0000

    Add sauna model with capacity, pricing, and availibilty

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Sauna(models.Model): # what can be booked, represents a physical sauna room
+    name  = models.CharField(max=length=100)
+    capacity = models.InterField(default=6)
+    description = models.TextField()
+    price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
+    is-active = models.BooleanField9deafault=True
+
+    def__str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        
