597360e5f2289d7632981f68b51d19b4026f03f3
Court-Pencil <courtneyhulse97@outlook.com>
2026-01-30 11:07:01 +0000

    Create Django superuser and apply initial database migrations

--- breathe_config/settings.py
+++ breathe_config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'bookings',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -75,9 +76,13 @@ WSGI_APPLICATION = 'breathe_config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),        
+        'USER': config('DB_USER'),        
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
