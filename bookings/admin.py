ce97caff2a4a853ab3ce4a731b6078f5410d879d
Court-Pencil <courtneyhulse97@outlook.com>
2026-01-30 13:02:36 +0000

    Register admin models, standardize date/time formatting, and validate with tests

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Sauna, TimeSlot, Booking
 
-# Register your models here.
+@admin.register(Sauna)
+class SaunaAdmin(admin.ModelAdmin):
+    # Customize how Saunas appear in admin panel
+    list_display = ['name', 'capacity', 'price_per_hour', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name', 'description']
+    list_editable = ['is_active']
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    # Show TimeSlot in admin and customize it.
+    list_display = ['start_time', 'end_time', 'is_available']
+    list_filter = ['is_available']
+    list_editable = ['is_available']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    # Customize how Bookings appear in admin panel
+    list_display = ['user', 'booking_date', 'time_slot', 'status', 'created_at' ]
+    list_filter = ['status', 'booking_date', 'sauna']
+    search_fields = ['user__username', 'user__email'] # follow the foreign key to user, then search username 
+    date_hierarchy = 'booking_date' # Lets you browse bookings by date, year or month
+
+    fieldsets = (
+        ('Booking Information', {
+            'fields': ('user', 'sauna', 'time_slot', 'booking_date')
+        }),
+        ('Details', {
+            'fields': ('number_of_guests', 'special_requests', 'status')
+        }),
+        ('MetaData', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+        )
+    readonly_fields = ['created_at', 'updated_at']
+    
+
+
+
+
+
--- breathe_config/settings.py
+++ breathe_config/settings.py
@@ -109,14 +109,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-gb'
+TIME_ZONE = "Europe/London"
 
 USE_I18N = True
-
 USE_TZ = True
 
+USE_L10N = False
+TIME_FORMAT = "H:i"
+DATETIME_FORMAT = "d/m/Y H:i"
+DATE_FORMAT = "d/m/Y"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
