ee6c77071cfa64f648e75c12c3290b905ca36dfa
Court-Pencil <courtneyhulse97@outlook.com>
2026-02-06 22:55:40 +0000

    add allowed hosts urls, incl heroku url

--- breathe_config/settings.py
+++ breathe_config/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sauna-project-ad449af7f2fb.herokuapp.com",
+    "localhost",
+    "127.0.0.1",]
 
 
 # Application definition
